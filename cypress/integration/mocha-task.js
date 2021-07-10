829f4c65e9cae4bbdff58cd93decbab5b65f2450
talaamin <76010372+talaamin@users.noreply.github.com>
2021-07-10 21:21:15 +0300

    mocha task

---
+++ cypress/integration/mocha-task.js
@@ -0,0 +1,19 @@
+const cartItems = ["Orange", "Banana", "Apple", "Strawberry"];
+it("cart should contain 4 items", () => {
+  assert.equal(cartItems.length, 4);
+  console.log(cartItems.length);
+});
+
+it("second item in the cart should be Banana", () => {
+  assert.equal(cartItems[1], "Banana");
+  console.log(cartItems[1]);
+});
+
+it("items should be sorted alphabetically", () => {
+  assert.equal(cartItems.sort(), "Apple,Banana,Orange,Strawberry");
+  console.log(cartItems.sort());
+});
+it("number of items should become 5 after adding Mango", () => {
+  cartItems.push("Mango");
+  assert.equal(cartItems.length, 5);
+});
